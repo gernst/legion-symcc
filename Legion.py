ad5d0d82588d48b7cccfe2d3da99bb85af5f2285
Gidon Ernst <gidon.ernst@sosy.ifi.lmu.de>
2021-09-20 16:33:45 +0200

    some working prototype with trivial selection heuristic

--- Legion.py
+++ Legion.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
 
 import z3
-import re
 import argparse
 import functools
 import itertools
 import random
 import bvsampler
 import subprocess as sp
-
-MAX_LEVEL   = 1
-DECLARE_FUN = re.compile('\(declare-fun (stdin[0-9]+) \(\) \(_ BitVec ([0-9]+)\)\)')
-
 
 def target_from_file(trace):
     with open(trace, "rt") as stream:
@@ -46,26 +41,27 @@ def trace_from_file(trace):
         decls    = {}
 
         polarity = None
+        site     = None
         pending  = []
 
         result   = []
 
         def flush():
-            if polarity is not None:
+            if pending:
                 ast = "(assert " + " ".join(pending) + ")"
                 assert(ast)
 
+                # parse_smt2_* returns a list
                 constraint, = z3.parse_smt2_string(ast, decls = decls)
-                if polarity is False:
-                    constraint = z3.Not(constraint)
-
-                event = (target, constraint)
+                event       = (site, target, polarity, constraint)
                 result.append(event)
 
                 pending.clear()
 
         for line in stream.readlines():
             line = line.strip()
+            if not line:
+                continue
 
             if line.startswith('in  '):
                 flush()
@@ -86,12 +82,12 @@ def trace_from_file(trace):
             elif line.startswith('yes '):
                 flush()
                 polarity = True
-                pending.append(line[4:])
+                site     = int(line[4:])
 
             elif line.startswith('no '):
                 flush()
                 polarity = False
-                pending.append(line[4:])
+                site     = int(line[4:])
 
             else:
                 pending.append(line)
@@ -101,54 +97,68 @@ def trace_from_file(trace):
 
 
 class Node:
-    def __init__(self, trace):
+    def __init__(self, target, constraints, parent = None):
+        self.site        = None
+        self.target      = target
+        self.nbytes      = target.size() if target is not None else 0
+        self.constraints = constraints
+        self.depth       = len(constraints)
+
+        self.parent      = parent
         self.children    = {}
-        self.constraints = []
-        self.target      = None
+        
         self.sampler     = None
 
-        for _, constraint in trace:
-            self.constraints.append(constraint)
+        self.phantom     = True
+        self.selected    = 0
 
-    def insert(self, index, trace):
-        if index == len(trace):
-            return None # leaf node, no new subtree
-        else:
-            phi = trace[index]
-            if phi in self.children:
-                child = self.children[phi]
-                return child.insert(index + 1, trace)
+    def insert(self, trace, constraints=[], index=0):
+        if index < len(trace):
+            site, target, polarity, phi = trace[index]
+
+            yes = phi
+            no  = z3.Not(phi)
+            phi = yes if polarity else no
+
+            if self.site:
+                assert self.site == site
             else:
-                child = Node(trace[:index])
-                self.children[phi] = child
-                child.insert(index + 1, trace)
-                return child # return root of new subtree
+                self.site = site
+            
+            if not self.children:
+                self.children[True]  = Node(target, constraints + [yes], parent=self)
+                self.children[False] = Node(target, constraints + [no],  parent=self)
+            
+            child  = self.children[polarity]
+            result = child.insert(trace, constraints + [phi], index + 1)
+        else:
+            result = None
+
+        if self.phantom:
+            self.phantom = False
+            return self
+        else:
+            return result
 
     def sample(self):
-        if not self.target:
+        if self.target is None:
             return b'' # no bytes to sample
 
-        if not self.sampler:
+        if self.sampler is None:
             solver = z3.Optimize()
-            self.sampler = bvsampler.bvsampler(solver, target)
+            self.sampler = bvsampler.bvsampler(solver, self.target)
 
-        sample = self.sampler.next()
-        return int_to_bytes(sample)
+        sample = next(self.sampler)
+        return int_to_bytes(sample, self.nbytes)
 
     def select(self):
-        if not self.children():
+        if not self.children:
             return self
         elif random_bit():
             return self
         else:
             child = random.choice(self.children)
             return child.select()
-
-
-def insert(root, trace):
-    child = root.insert(0, trace)
-    print("found new path")
-    return child
 
 
 def int_to_bytes(value, nbytes):
@@ -208,6 +218,9 @@ def compile_c(source_c, binary):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Legion')
+    parser.add_argument("-c", "--compile",
+                        action='store_true',
+                        help='force compile')
     parser.add_argument("file",
                         help='C source file')
     args     = parser.parse_args()
@@ -217,17 +230,30 @@ if __name__ == '__main__':
     assert source_c[-2:] == '.c'
     binary   = "./" + source_c[:-2]
 
-    compile_c(source_c, binary)
+    if args.compile:
+        compile_c(source_c, binary)
 
-    code, outs, errs, log = execute_with_input(binary, bytes(), 'traces', 1)
-    write_testcase(outs, 'tests', 1)
+    root = Node(None, [])
 
-    trace = trace_from_file(log)
-    for foo in trace:
-        print(foo)
+    for i in range(1,11):
+        # print("round", i)
+        node   = root.select()
+        node.selected += 1
+        # print("depth", node.depth, " #selected ", node.selected, "select", node.site)
+        
+        prefix = node.sample()
+        
+        code, outs, errs, log = execute_with_input(binary, prefix, 'traces', i)
+        trace = trace_from_file(log)
+        added = root.insert(trace)
+        _, _, path, _ = zip(*trace)
 
-    root = Node([])
-    insert(root, trace)
+        if added:
+            write_testcase(outs, 'tests', i)
+            print("new: ", path)
+        else:
+            # print("old: ", path)
+            pass
 
     # target = target_from_file(trace)
     # print(target)
